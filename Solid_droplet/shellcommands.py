3508cd8e8c7c89ff625ad8bb059b7325a1a4cca1
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-04 17:04:22 +0200

    Add python implementation (does not work fully)

--- Solid_droplet/shellcommands.py
+++ Solid_droplet/shellcommands.py
@@ -5,11 +5,12 @@ PATH_BLENDER_FOLDER = environ["Blender"]
 PATH_BLENDER = path.join(PATH_BLENDER_FOLDER, "blender") 
 
 # Get code path location
-CODE_PATH = getcwd()
+CODE_PATH = path.join(getcwd(), "Solid_droplet")
 
 # Find droplet .blend file (test.blend)
 try:
-    BLEND_FILE_PATH = path.join(CODE_PATH, "test.blend1")
+    #BLEND_FILE_PATH = path.join(CODE_PATH, "test.blend1")
+    BLEND_FILE_PATH = path.join(CODE_PATH, "test.blend")
 except:
     print("File has not been found check if file is correct")
 
@@ -21,3 +22,7 @@ system(f"{PATH_BLENDER} -b {BLEND_FILE_PATH} -o {DATA_PATH} -f 10")
 # -o: accepts storage location
 # -b: background (does not open blender)
 # -f: Only a single frame (we don't need multiple, since we don't have an animation).
+
+# If we have Python code implentation outside we can use this:
+# system(f"{PATH_BLENDER} -b -P {BLEND_FILE_PATH} -o {DATA_PATH} -f 10")
+# -P: calls a python file.

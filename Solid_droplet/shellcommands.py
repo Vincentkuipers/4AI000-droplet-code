64f04cb63c7db927d0534cae26c56c8b85e4a100
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-09 11:20:18 +0200

    clean up code

--- Solid_droplet/shellcommands.py
+++ Solid_droplet/shellcommands.py
@@ -6,33 +6,19 @@ try:
     PATH_BLENDER = path.join(PATH_BLENDER_FOLDER, "blender") 
     print("Blender Directory was found")
 except:
-    print("Directory not found -> try checking if the directory is correctly specified")
+    raise NameError("Directory not found -> try checking if the directory is correctly specified")
 
-
-# Get code path location
-CODE_PATH = getcwd()
-
-# Find droplet .blend file (test.blend)
+# Find code file
 try:
-    # Blender location
-    BLEND_FILE_PATH = path.join(CODE_PATH, "empty.blend")
+    CODE_PATH = getcwd()
     BLEND_FILE_CODE = path.join(CODE_PATH, "CurrentBlenderCode.py")
     print("Files were found")
-    # BLEND_FILE_PATH = path.join(CODE_PATH, "BlenderTest.blend")
 except:
-    print("File has not been found check if file is correct")
-
-# Storage file location
-DATA_PATH = path.join(CODE_PATH, "Data\\")
-
-# Code injection into powershell
-# system(f"{PATH_BLENDER} -b {BLEND_FILE_PATH} -o {DATA_PATH} -f 10")
-# -o: accepts storage location -> predefined in CurrentBlenderCode.py
-# -b: background (does not open blender) -> speeds up things a ton.
-# -f: Only a single frame (we don't need multiple, since we don't have an animation). -> set up such that only one frame is rendered.
+    raise NameError("File has not been found check if file is correct")
 
 # If we have Python code implentation outside we can use this:
-sigma = 90
+sigma = 68
 system(f"{PATH_BLENDER} -b -P {BLEND_FILE_CODE} -- {sigma}")
+# -b: background (does not open blender) -> speeds up things a ton.
 # -P: calls a python file
 # -- passes system arguments, needed for sigma, volume, rneedle

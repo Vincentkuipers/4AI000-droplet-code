256e0bf013efac926e65a8c8e25a9b7bcbcc16fd
lexhoevenaars <60692812+lexhoevenaars@users.noreply.github.com>
2023-05-18 17:17:36 +0200

    Removed line, also in python script

--- Solid_droplet/CurrentBlenderCode.py
+++ Solid_droplet/CurrentBlenderCode.py
@@ -9,7 +9,6 @@ import math
 import bmesh
 from bmesh.ops import spin
 
-
 dir = os.path.dirname(bpy.data.filepath)
 if not dir in sys.path:
     sys.path.append(dir )
@@ -18,6 +17,12 @@ from fun_genSingleDrop import *
 sigma = int(sys.argv[-1])
 #volume = sys.argv
 #rneedle = sys.argv
+
+for ob in bpy.data.objects:
+    if ob.name in ["Light"]:
+        bpy.data.objects[ob.name].select_set(True)
+        bpy.ops.object.delete()
+
 
 r_a, z_a = genSingleDrop(sigma, 32, 1,1 )
 
@@ -47,9 +52,9 @@ new_mesh = bpy.data.meshes.new("droplet_mesh")
 new_mesh.from_pydata(edge_points, edges, [])
 new_mesh.update()
 
+
 new_object = bpy.data.objects.new("droplet_object", new_mesh)
-
-
+bpy.context.scene.collection.objects.link(new_object)
 
 def lathe_geometry(bm, cent, axis, dvec, angle, steps, remove_doubles=True, dist=0.0001):
     geom = bm.verts[:] + bm.edges[:]
@@ -68,6 +73,7 @@ def lathe_geometry(bm, cent, axis, dvec, angle, steps, remove_doubles=True, dist
     if remove_doubles:
         bmesh.ops.remove_doubles(bm, verts=bm.verts[:], dist=dist)
 
+
 obj = bpy.data.objects["droplet_object"]
 bm = bmesh.new()
 bm.from_mesh(obj.data)
@@ -76,14 +82,15 @@ axis = (0,0,1)
 dvec = (0,0,0)
 angle = 2*math.pi
 steps = 60
-cent = obj.location
+cent =  obj.location
+
+
 
 lathe_geometry(bm, cent, axis, dvec, angle, steps, remove_doubles=True, dist=0.0001)
 
 bm.to_mesh(obj.data)
 # obj.data.update()   # if you want update to show immediately
 bm.free()
-bpy.context.scene.collection.objects.link(obj)
 
 # Set the background color to blue
 # bpy.context.scene.world.node_tree.nodes['Background'].inputs[0].default_value = (0.0, 0.0, 1.0, 1.0)
@@ -94,7 +101,7 @@ world_node_tree = bpy.context.scene.world.node_tree
 environment_texture_node = world_node_tree.nodes.new(type='ShaderNodeTexEnvironment')
 
 # Load the background image
-background_image = bpy.data.images.load('C:/4AI000/Solid_droplet/TUE_background.jpg')
+background_image = bpy.data.images.load('C:/4AI000/Solid_droplet/scythian_tombs_2_4k.exr')
 
 # Set the image as the texture for the Environment Texture node
 environment_texture_node.image = background_image
@@ -104,25 +111,28 @@ background_node = world_node_tree.nodes['Background']
 world_node_tree.links.new(environment_texture_node.outputs['Color'], background_node.inputs['Color'])
 
 
-
 # Add a subdivision surface modifier
 subsurf = obj.modifiers.new(name='Subdivision Surface', type='SUBSURF')
-subsurf.levels = 2  # Increase the number of subdivision levels
+subsurf.levels = 2 # Increase the number of subdivision levels
+
 
 # Create a new water material
+water_material = bpy.data.materials.new(name="Water")
+water_material.use_nodes = True
+
 water_material = bpy.data.materials.new(name="Water")
 water_material.use_nodes = True
 
 # Get a reference to the Principled BSDF node in the material's node tree
 principled_bsdf_node = water_material.node_tree.nodes["Principled BSDF"]
 
-# Set the roughness of the material to 0.05
+# Set the roughness of the material to 0.2
 principled_bsdf_node.inputs["Roughness"].default_value = 0.2
 
-# Set the transmission to 0.5 (partial transparency)
+# Set the transmission to 1.0 (full transparency)
 principled_bsdf_node.inputs['Transmission'].default_value = 1.0
 
-# Set the alpha to 0.5 (partial transparency)
+# Set the alpha to 1.0 (full transparency)
 principled_bsdf_node.inputs['Alpha'].default_value = 1.0
 
 # Set the IOR to 1.33 (refractive index of water)
@@ -131,40 +141,19 @@ principled_bsdf_node.inputs['IOR'].default_value = 1.33
 # Set the color of the material to white
 principled_bsdf_node.inputs["Base Color"].default_value = (0.8, 0.9, 1.0, 1.0)
 
-# Add a Glossy BSDF node to the material's node tree
-glossy_bsdf_node = water_material.node_tree.nodes.new(type='ShaderNodeBsdfGlossy')
-glossy_bsdf_node.inputs['Color'].default_value = (1.0, 1.0, 1.0, 1.0)
-glossy_bsdf_node.inputs['Roughness'].default_value = 0.1
-
-# Add a Mix Shader node to the material's node tree
-mix_shader_node = water_material.node_tree.nodes.new(type='ShaderNodeMixShader')
-mix_shader_node.inputs['Fac'].default_value = 0.5
-
-# Connect the Principled BSDF node to the Mix Shader node
-water_material.node_tree.links.new(principled_bsdf_node.outputs['BSDF'], mix_shader_node.inputs[1])
-
-# Connect the Glossy BSDF node to the Mix Shader node
-water_material.node_tree.links.new(glossy_bsdf_node.outputs['BSDF'], mix_shader_node.inputs[2])
-
-# Set the output of the material to the Mix Shader node
-water_material.node_tree.links.new(mix_shader_node.outputs['Shader'], water_material.node_tree.nodes['Material Output'].inputs['Surface'])
-
-# Set the blend mode to 'Alpha Blend'
-water_material.blend_method = 'BLEND'
-
-# Assign the water material to the object
+# Create a new material slot for the object
 obj.data.materials.append(water_material)
 
-########## Add a new point light to the scene
-light_data = bpy.data.lights.new(name="New Light", type='POINT')
-light_obj = bpy.data.objects.new(name="New Light", object_data=light_data)
-bpy.context.scene.collection.objects.link(light_obj)
+# ########## Add a new point light to the scene
+# light_data = bpy.data.lights.new(name="New Light", type='POINT')
+# light_obj = bpy.data.objects.new(name="New Light", object_data=light_data)
+# bpy.context.scene.collection.objects.link(light_obj)
 
-# Set the location of the light
-light_obj.location = (-2, -3, 4.0)
+# # Set the location of the light
+# light_obj.location = (-2, -3, 4.0)
 
-# Set the intensity of the light
-light_data.energy = 100.0
+# # Set the intensity of the light
+# light_data.energy = 100.0
 
 ##########Get the active camera object
 camera = bpy.context.scene.camera
@@ -175,10 +164,17 @@ camera = bpy.context.scene.camera
 
 # Set the camera location and rotation
 camera.location = (7.3589*1.5,-6.9258*1.5, 4.9583)         #x, y, z are the desired location values
-camera.rotation_euler = (1.1093*1.25, 0, 0.8149)           # rx, ry, rz are the rotation values in radians
+camera.rotation_euler = (1.1093*1.25, 0, 0.8149*1.2)           # rx, ry, rz are the rotation values in radians
 
 
 bpy.context.scene.frame_end = 0
 #bpy.context.scene.render.file_extension = "PNG"
 bpy.context.scene.render.filepath = f"//Data//{sigma}"
 bpy.ops.render.render(write_still = True)
+
+#         ### Clear Scene except for Light and Camera objects
+# for ob in bpy.data.objects:
+#     if ob.name not in ["Camera","Light"]:
+#         bpy.data.objects[ob.name].select_set(True)
+#         bpy.ops.object.delete()
+

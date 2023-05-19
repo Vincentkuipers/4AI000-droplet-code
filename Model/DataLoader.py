4d32bb54f5c7c6a5d3b9b8c916aecc63c8c80d6a
Dhuige <31347265+Dhuige@users.noreply.github.com>
2023-05-19 17:26:26 +0200

    Add load images functions

--- Model/DataLoader.py
+++ Model/DataLoader.py
@@ -1,11 +1,18 @@
+from cv2 import imread, resize, cvtColor, COLOR_BGR2GRAY
 from random import seed, shuffle
-from os import listdir
-from numpy import array
+from numpy import array, float32
+from os import listdir, path
 
 class DataLoader():
-    """Class for loading data and the labels"""
+    """Class for loading images and the labels"""
     def __init__(self, DATA_DIR:list, train_frac:float = 0.6, val_frac:float = 0.2, test_frac:float=0.2, use_seed:bool=False, seed_val:int=42):
+        # Split the data into train, validation and test set
         train, val, test, trainl, vall, testl = self._split_data(DATA_DIR, train_frac, val_frac, test_frac, use_seed, seed_val)
+        
+        # Load images
+        train, val, test = self._load_images(train, val, test, DATA_DIR)
+
+        # Store data
         self.train = train
         self.val = val
         self.test = test
@@ -13,6 +20,14 @@ class DataLoader():
         self.val_label = vall
         self.test_label = testl
 
+
+    def _load_images(self, train_split, val_split, test_split, DATA_DIR:str):
+        """Load images from DATA_DIR"""
+        train_images = self._get_images(train_split, DATA_DIR)
+        val_images = self._get_images(val_split, DATA_DIR)
+        test_images = self._get_images(test_split, DATA_DIR)
+        return train_images, val_images, test_images
+    
 
     def _split_data(self, DATA_DIR:list, train_frac:float = 0.6, val_frac:float = 0.2, test_frac:float=0.2, use_seed:bool=False, seed_val:int=42):
         """Split data into train, validation and test set"""
@@ -45,11 +60,12 @@ class DataLoader():
         val_split = list_of_labels[train_value:train_value+val_value]
         test_split = list_of_labels[train_value+val_value:]
 
+        # Get labels
         train_labels = self._get_labels(train_split)
         val_labels = self._get_labels(val_split)
         test_labels = self._get_labels(test_split)
 
-        return train_split, val_split, test_split , train_labels, val_labels, test_labels
+        return train_split, val_split, test_split, train_labels, val_labels, test_labels
 
         
     def _get_labels(self, list:list):
@@ -71,3 +87,30 @@ class DataLoader():
         label = array(label)                    
         return label
 
+    def _image_resize(self, image, size=(320,256)):
+        """Resize image to 320x256"""
+        return resize(image, size)
+
+    def _image_color(self, image):
+        """Convert image from RGB to GRAY"""
+        return cvtColor(image, COLOR_BGR2GRAY)
+    
+    def _image_normalize(self, image):
+        """Normalize image"""
+        return image.astype(float32)/255.0
+    
+    def _image_processing(self, image):
+        """Process image"""
+        image = self._image_resize(image)
+        image = self._image_color(image)
+        image = self._image_normalize(image)
+        return image
+
+    def _get_images(self, list_of_labels:list, DATA_DIR:str):
+        """Get images from list"""
+        images = []
+        for filename in list_of_labels:
+            image = imread(path.join(DATA_DIR, filename))
+            image = self._image_processing(image)
+            images.append(image)
+        return images

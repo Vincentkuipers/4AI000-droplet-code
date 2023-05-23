906f857965f0d6ff4b13dd517c2d46615535f202
Vincent <v.b.f.kuipers@student.tue.nl>
2023-05-23 12:24:18 +0200

    Added smaller steps

--- Solid_droplet/shellcommands.py
+++ Solid_droplet/shellcommands.py
@@ -18,11 +18,11 @@ except:
     raise NameError("File has not been found check if file is correct")
 
 # note it will do sigma_min, sigma_min+1, ..., sigma_max-1 
-[sigma_min, sigma_max] = [40, 71] 
-[volume_min, volume_max] = [32, 51]
-[rneedle_min, rneedle_max] = [1,2]
+[sigma_min, sigma_max, sigma_step] = [66, 80, 0.25] 
+[volume_min, volume_max, volume_step] = [32, 33, 1]
+[rneedle_min, rneedle_max, rneedle_step] = [1,2,1]
 
-system(f"{PATH_BLENDER} -b {BLEND_BLEND_FILE} -P {BLEND_FILE_CODE} -- {sigma_min} {sigma_max} {volume_min} {volume_max} {rneedle_min} {rneedle_max}")
+system(f"{PATH_BLENDER} -b {BLEND_BLEND_FILE} -P {BLEND_FILE_CODE} -- {sigma_min} {sigma_max} {sigma_step} {volume_min} {volume_max} {volume_step} {rneedle_min} {rneedle_max} {rneedle_step}")
 # -b: background (does not open blender) -> speeds up things a ton.
 # -P: calls a python file
 # -- passes system arguments, needed for sigma, volume, rneedle
